2ae4a8d223f4ae3c6f56f44040afb535fc615c13
Vadim Sabirov <pr0head@gmail.com>
2019-03-04 18:57:56 +0300

    Change userinfo endpoint url

--- src/getFunctionalUrls.js
+++ src/getFunctionalUrls.js
@@ -2,7 +2,7 @@ export default function getFunctionalUrls(apiUrl = 'https://auth1.tst.protocol.o
   return {
     apiUrl,
     apiLoginUrl: `${apiUrl}/oauth2/login`,
-    apiUserinfoUrl: `${apiUrl}/userinfo`,
+    apiUserinfoUrl: `${apiUrl}/oauth2/userinfo`,
     apiRegisterUrl: `${apiUrl}/oauth2/signup`,
     apiSocialAuthUrl: `${apiUrl}/authorize`,
     apiSocialAuthLinkUrl: `${apiUrl}/authorize/link`,
